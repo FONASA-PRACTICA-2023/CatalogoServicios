e1b9710b94cf9f05d810acc8397ad4615f856bfa
miguel.hernandez <miguel.hernandez@fonasa.cl>
2023-07-26 17:30:11 +0000

    ede

--- src/pages/servicios/inicioClaveUnica.jsx
+++ src/pages/servicios/inicioClaveUnica.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 const MyComponent = () => {
   const [response, setResponse] = useState(null);
+  const [datosUsuario, setDatosUsuario] = useState([]);
 
   useEffect(() => {
     // Obtener los valores de la URL
@@ -32,15 +33,46 @@ const MyComponent = () => {
     fetch("https://accounts.claveunica.gob.cl/openid/token/", requestOptions)
       .then(response => response.text())
       .then(result => setResponse(result))
+      getData()
       .catch(error => console.log('error', error));
   }, []);
 
-  return (
-    <div className="container w-50 mt-4">
-      <h1 className="mb-3">Respuesta del POST</h1>
-      <pre>{response}</pre>
-    </div>
-  );
-};
 
-export default MyComponent;
+
+  const getData = async () => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", response.access_token);
+    myHeaders.append("Cookie", "csrftoken=GNgYNAH991h5IR0qeggMXI4bFmvo28k1WvW4WDnVMVQEhOdDWhA7Nf03IfvUC7CD");
+
+    var graphql = JSON.stringify({
+      query: "",
+      variables: {}
+    })
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: graphql,
+      redirect: 'follow'
+    };
+
+    fetch("https://accounts.claveunica.gob.cl/openid/userinfo", requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .then(result => setDatosUsuario(result))
+      
+      .catch(error => console.log('error', error));
+
+  }
+
+    return (
+      <div className="container w-50 mt-4">
+        <h1 className="mb-3">Respuesta del POST</h1>
+        <pre>{response}</pre>
+        <h1 className="mb-3">Datos usuario</h1>
+        <pre>{datosUsuario}</pre>
+      </div>
+    );
+  };
+
+  export default MyComponent;
